aeb0e3fc1b1a7ae5bdf8005c1dd49529e8e83b4a
Akonit44 <for_iphone91@mail.ru>
2019-11-23 15:40:59 +0300

    Сохранил код для вывода html в строку

---
+++ Table_of_rastr/Table_to_database/Table_to_database/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string URL = "http://www.hemi.nsu.ru/rastab.htm";
+            if (URL == "" || URL == " ")
+            {
+                Console.WriteLine("Невозможно отправить GET запрос к адресу", URL);
+            }
+            else
+            {
+                //Отправляем запрос,где textBox1 - строка с адресом
+
+                System.Net.WebRequest reqGET = System.Net.WebRequest.Create(URL);
+                System.Net.WebResponse resp = reqGET.GetResponse();
+                System.IO.Stream stream = resp.GetResponseStream();
+                //Получаем ответ в переменную sr и считываем его до конца
+                System.IO.StreamReader sr = new System.IO.StreamReader(stream, Encoding.Default);
+                string s = sr.ReadToEnd();
+                //Выводим всю лабуду в richTextBox1
+                URL = s;
+            }
+            Console.WriteLine(URL);
+            Console.ReadLine();
+        }
+    }
+}
