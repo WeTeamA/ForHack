920b74af830c28918f5729134af066ce8e9f316e
Rexagon <reide740@gmail.com>
2019-11-23 16:55:13 +0300

    Complete parse 3 params

--- Assets/Request.cs
+++ Assets/Request.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.Xml;
 using System.Net;
 using System.IO;
+using System.Text.RegularExpressions;
 
 public class Substance
 {
@@ -22,12 +23,118 @@ public class Substance
 }
 public class Request : MonoBehaviour
 {
+    /// <summary>
+    /// Если Solid -> true, Если Liquid -> false
+    /// </summary>
+    /// <param name="childnode"></param>
+    /// <returns></returns>
+    public bool Aggregate(XmlNode childnode)
+    {
+        Regex regex = new Regex(@"solid");
+        MatchCollection matches = regex.Matches(childnode.InnerText);
+        if (matches.Count > 0)
+        {
+            if (matches[0].Value == "solid")
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        else
+            return false;
+    }
 
+    /// <summary>
+    /// Soluble -> true, Если Unsoluble -> false
+    /// </summary>
+    /// <param name="childnode"></param>
+    /// <returns></returns>
+    public bool Solubility(XmlNode childnode)
+    {
+        Regex regex = new Regex(@"soluble");
+        MatchCollection matches = regex.Matches(childnode.InnerText);
+        if (matches.Count > 0)
+        {
+            if (matches[0].Value == "soluble")
+                return true;
+            else
+            {
+                return false;
+            }
+        }
+        else
+            return false;
+    }
+
+    public double Temp(XmlNode childnode)
+    {
+        Regex regex = new Regex(@"\d*\s°C");
+        MatchCollection matches = regex.Matches(childnode.InnerText);
+        if (matches.Count > 0)
+        {
+            string x = matches[0].Value;
+            int x1 = x.Length - 3;
+            x = x.Remove(x1);
+            return double.Parse(x);
+        }
+        else
+            return 100;
+    }
+
+    public void get_http_write(string url)
+    {
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+        //req.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0";
+        //req.CookieContainer = cookies;
+        //req.Headers.Add("DNT", "1");
+
+        HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+        Stream stream = resp.GetResponseStream();
+        StreamReader sr = new StreamReader(stream);
+        //resp.Close();
+        string text = sr.ReadToEnd();
+        // sr.Close();
+        using (var sw = new StreamWriter("page.xml"))
+            sw.Write(text); //пишем на комп
+        resp.Close();
+        sr.Close();
+    }
+
+    public void FillArray(List<Substance> Subs)
+    {
+        var doc = new XmlDocument();
+        doc.Load("C:/ForHack/ForHack/page.xml");
+
+        XmlElement xRoot = doc.DocumentElement;
+        foreach (XmlNode childnode in xRoot)
+        {
+            if (childnode.Name == "pod" && childnode.Attributes.GetNamedItem("title").Value == "Basic properties")
+            {
+                foreach (XmlNode childnode2 in childnode.ChildNodes)
+                {
+                    if (childnode2.Name == "subpod")
+                    {
+                        foreach (XmlNode childnode3 in childnode.ChildNodes)
+                        {
+                            if (childnode3.Name == "subpod")
+                            {
+                                foreach (XmlNode childnode4 in childnode3.ChildNodes)
+                                {
+                                    if (childnode4.Name == "plaintext")
+                                    {
+                                        Subs.Add(new Substance(null, Solubility(childnode4), Temp(childnode4), Aggregate(childnode4)));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
     // Start is called before the first frame update
     void Start()
     {
         List<Substance> Substances = new List<Substance>();
-        //Substances.Add(new Substance("asdf", true, 454, false));
         //APP NAME: ChemicalLab
         //APPID: K58ETV - GTPAJVATGW
         // var url = "http://api.wolframalpha.com/v2/query?input=NaCl&appid=K58ETV-GTPAJVATGW";
@@ -36,88 +143,46 @@ public class Request : MonoBehaviour
         //get_http_write("http://api.wolframalpha.com/v2/query?input=NaCl&appid=K58ETV-GTPAJVATGW"); ЭТО ПОКА НЕ НУЖНОООООО
 
 
-        var doc = new XmlDocument();
-        doc.Load("C:/ForHack/ForHack/page.xml");
 
-        XmlElement xRoot = doc.DocumentElement;
-        foreach (XmlNode childnode in xRoot)
-        {
-                if (childnode.Name == "pod" && childnode.Attributes.GetNamedItem("title").Value == "Basic properties")
+
+
+                /*
+                for (int i = 0; i < Substances.Count; i++) //Заполнение массива
                 {
-                    foreach (XmlNode childnode2 in childnode.ChildNodes)
-                    {
-                        if (childnode2.Name == "subpod")
-                        {
-                            foreach (XmlNode childnode3 in childnode.ChildNodes)
-                            {
-                                if (childnode3.Name == "subpod")
-                                {
-                                foreach (XmlNode childnode4 in childnode3.ChildNodes)
-                                {
-                                    if (childnode4.Name == "plaintext")
-                                    {
-                                        print(childnode3.InnerText);
-                                    }
-                                }
-                                }
-                            }
-                        }
-                    }
+                    //Substances[i].temp = 
                 }
-            
+                //print(get_http("http://api.wolframalpha.com/v2/query?input=NaCl&appid=HP44EW-XHHUV3698T"));
+                */
+            }
 
             /*
-            for (int i = 0; i < Substances.Count; i++) //Заполнение массива
+            //CookieContainer cookies = new CookieContainer();
+            private string get_http(string url)
             {
-                //Substances[i].temp = 
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                //req.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0";
+                //req.CookieContainer = cookies;
+                //req.Headers.Add("DNT", "1");
+
+                HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+                Stream stream = resp.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+                resp.Close();
+                string text = sr.ReadToEnd();
+                sr.Close();
+                return text;
+
             }
-            //print(get_http("http://api.wolframalpha.com/v2/query?input=NaCl&appid=HP44EW-XHHUV3698T"));
             */
-        }
 
-        /*
-        //CookieContainer cookies = new CookieContainer();
-        private string get_http(string url)
-        {
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-            //req.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0";
-            //req.CookieContainer = cookies;
-            //req.Headers.Add("DNT", "1");
 
-            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
-            Stream stream = resp.GetResponseStream();
-            StreamReader sr = new StreamReader(stream);
-            resp.Close();
-            string text = sr.ReadToEnd();
-            sr.Close();
-            return text;
+           
 
-        }
-        */
+            // Update is called once per frame
+            void Update()
+            {
 
-        void get_http_write(string url)
-        {
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-            //req.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0";
-            //req.CookieContainer = cookies;
-            //req.Headers.Add("DNT", "1");
-
-            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
-            Stream stream = resp.GetResponseStream();
-            StreamReader sr = new StreamReader(stream);
-            //resp.Close();
-            string text = sr.ReadToEnd();
-            // sr.Close();
-            using (var sw = new StreamWriter("page.xml"))
-                sw.Write(text); //пишем на комп
-            resp.Close();
-            sr.Close();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+            }
         }
     }
 }
